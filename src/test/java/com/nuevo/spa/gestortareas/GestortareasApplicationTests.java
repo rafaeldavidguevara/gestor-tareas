c64f0b02b572c87abc7146c63b0622aed973891b
rguevara <rafaeldavidguevara@gmail.com>
2025-07-05 23:13:49 -0400

    commit inicial

---
+++ src/test/java/com/nuevo/spa/gestortareas/GestortareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuevo.spa.gestortareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestortareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
