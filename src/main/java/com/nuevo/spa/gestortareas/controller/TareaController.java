321f92d8dbb0f1e0860c9a7147ca0d38b048b9f3
rafaeldavidguevara <rafaeldavidguevara@gmail.com>
2025-07-07 12:29:55 -0400

    endpoints de tarea documentados

--- src/main/java/com/nuevo/spa/gestortareas/controller/TareaController.java
+++ src/main/java/com/nuevo/spa/gestortareas/controller/TareaController.java
@@ -4,48 +4,75 @@ import com.nuevo.spa.gestortareas.service.TareaService;
 import com.nuevo.spa.gestortareas.util.dto.TareaCambioDto;
 import com.nuevo.spa.gestortareas.util.dto.TareaDto;
 import com.nuevo.spa.gestortareas.util.dto.TareaOutputDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tarea")
+@Tag(name = "API de Tareas")
 public class TareaController {
     private final TareaService tareaService;
 
     public TareaController(TareaService tareaService) {
         this.tareaService = tareaService;
     }
-
-    @GetMapping(value = "/obtenerTodas")
+    @Operation(summary = "Mostrar todas las tareas", description = "Retorna una lista con todas las tareas en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tareas mostradas exitosamente")
+    })
+    @GetMapping(value = "/obtenerTodas", produces = { "application/json" })
     public ResponseEntity<List<TareaOutputDto>> obtenerTareas() {
         return ResponseEntity.ok(tareaService.obtenerTareas());
     }
 
-    @GetMapping(value = "/obtener/{tareaId}")
+    @Operation(summary = "Mostrar una tarea dado su id", description = "Retorna una tarea en el sistema dado un id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea mostrada exitosamente")
+    })
+    @GetMapping(value = "/obtener/{tareaId}", produces = { "application/json" })
     public ResponseEntity<TareaOutputDto> obtenerTarea(@PathVariable("tareaId") Long id) {
         return ResponseEntity.ok(tareaService.obtenerTarea(id));
     }
 
-    @PostMapping(value = "/crear")
+    @Operation(summary = "Crear una tarea nueva", description = "Retorna una tarea nueva y la almacena en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea creada exitosamente")
+    })
+    @PostMapping(value = "/crear",produces = { "application/json" }, consumes = { "application/json" })
     public ResponseEntity<TareaOutputDto> crearTarea(@RequestBody TareaDto tareaNueva) {
         return ResponseEntity.ok(tareaService.crearTarea(tareaNueva));
     }
 
+    @Operation(summary = "Borrar una tarea del sistema dado su id", description = "Elimina una tarea en el sistema dado un id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea borrada exitosamente")
+    })
     @DeleteMapping(value = "/borrar/{tareaId}")
     public ResponseEntity<String> borrarTarea(@PathVariable("tareaId") Long id) {
         tareaService.borrarTarea(id);
         return ResponseEntity.ok("Tarea borrada exitosamente");
     }
 
-    @PutMapping(value = "/actualizar")
+    @Operation(summary = "Actualizar una tarea", description = "Actualiza una tarea en el sistema a partir de otra tarea")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea actualizada exitosamente")
+    })
+    @PutMapping(value = "/actualizar", produces = { "application/json" }, consumes = { "application/json" })
     public ResponseEntity<TareaOutputDto> actualizarTarea(@RequestBody TareaCambioDto tareaCambioDto) {
         return ResponseEntity.ok(tareaService.actualizarTarea(tareaCambioDto));
     }
 
-    @PostMapping(value = "/actualizarVarias")
+    @Operation(summary = "Actualizar varias tareas", description = "Actualiza varias tarea en el sistema a partir de un listado de tareas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea actualizada exitosamente")
+    })
+    @PostMapping(value = "/actualizarVarias", produces = { "application/json" }, consumes = { "application/json" })
     public ResponseEntity<List<TareaOutputDto>> actualizarTareas(@RequestBody List<TareaCambioDto> tareaCambioDto) {
         return ResponseEntity.ok(tareaService.actualizarTareas(tareaCambioDto));
     }
